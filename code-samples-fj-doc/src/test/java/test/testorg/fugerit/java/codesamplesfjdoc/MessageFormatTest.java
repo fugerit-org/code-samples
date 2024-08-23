8e3475c2b87ee0a5dfb64ff391c61325eaefd7e3
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-08-23 23:26:52 +0200

    Added messageFormat template

---
+++ code-samples-fj-doc/src/test/java/test/testorg/fugerit/java/codesamplesfjdoc/MessageFormatTest.java
@@ -0,0 +1,74 @@
+package test.testorg.fugerit.java.codesamplesfjdoc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fugerit.java.codesamplesfjdoc.DocHelper;
+import org.fugerit.java.doc.base.config.DocConfig;
+import org.fugerit.java.doc.base.process.DocProcessContext;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+/**
+ * This is a basic example of Fugerit Venus Doc usage,
+ * running this main the program will :
+ * - creates data to be used in document model
+ * - renders the 'document.ftl' template
+ * - print the result in markdown format on the log
+ *
+ * For further documentation :
+ * https://github.com/fugerit-org/fj-doc
+ */
+@Slf4j
+class MessageFormatTest {
+
+    @Test
+    void test() throws IOException {
+        try ( ByteArrayOutputStream baos = new ByteArrayOutputStream() ) {
+            // creates the doc helper
+            DocHelper docHelper = new DocHelper();
+            // create custom data for the fremarker template 'document.ftl'
+            Properties params = new Properties();
+            params.setProperty( "prop1", "The king of Rivendell is {0}" );
+            // handler id
+            String handlerId = DocConfig.TYPE_MD;
+            // output generation
+            docHelper.getDocProcessConfig().fullProcess( "message-format", DocProcessContext.newContext( "params", params ), handlerId, baos );
+            // print the output
+            log.info( "html output : \n{}", new String( baos.toByteArray(), StandardCharsets.UTF_8 ) );
+        }
+    }
+
+    /*
+     * Class used to wrap data to be rendered in the document template
+     */
+    public static class People {
+
+        private String name;
+
+        private String surname;
+
+        private String title;
+
+        public People(String name, String surname, String title) {
+            this.name = name;
+            this.surname = surname;
+            this.title = title;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+
+}
