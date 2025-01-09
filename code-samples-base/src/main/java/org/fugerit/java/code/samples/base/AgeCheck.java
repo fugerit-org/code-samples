788d06ac30e9bc028d3f4184ef0ac3e5a7a234b1
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-01-09 09:38:27 +0100

    Handling with Period

--- code-samples-base/src/main/java/org/fugerit/java/code/samples/base/AgeCheck.java
+++ code-samples-base/src/main/java/org/fugerit/java/code/samples/base/AgeCheck.java
@@ -1,6 +1,7 @@
 package org.fugerit.java.code.samples.base;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
 
 public class AgeCheck {
@@ -8,7 +9,7 @@ public class AgeCheck {
     private AgeCheck() {}
 
     public static Boolean isAgeAtLeast(LocalDate d, int numberOfYears) {
-        return d != null && d.until(LocalDate.now(), ChronoUnit.YEARS)>=numberOfYears;
+        return d != null &&  Period.between(d, LocalDate.now()).getYears() >=numberOfYears;
     }
 
     public static Boolean isAgeAtLeast18(LocalDate d) {
