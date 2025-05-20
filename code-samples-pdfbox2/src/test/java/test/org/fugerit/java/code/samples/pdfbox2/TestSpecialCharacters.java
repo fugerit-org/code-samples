cade8bc4b2961931dbda34113ad4e73235c5cd32
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-05-21 00:39:36 +0200

    Test check font

--- code-samples-pdfbox2/src/test/java/test/org/fugerit/java/code/samples/pdfbox2/TestSpecialCharacters.java
+++ code-samples-pdfbox2/src/test/java/test/org/fugerit/java/code/samples/pdfbox2/TestSpecialCharacters.java
@@ -4,10 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.fugerit.java.code.samples.pdfbox2.SpecialCharacters;
 import org.fugerit.java.core.lang.helpers.ClassHelper;
+import org.fugerit.java.core.util.PropsIO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * Proof of Concept to handle special characters like  U+2002 ('enspace')
@@ -34,13 +41,42 @@ class TestSpecialCharacters {
             "\u2212",   // 'minus'
             "\u0141" ,  // 'Lslash'
             "\u2103",   // 'centigrade'
+            "\u27E8",   // '⟨',
+            "\u27E9",   // '⟩',
     };
+
+    private static final String HELVETICA = "Helvetica";
+    private static final String DEJAVUSANS = "DejaVuSans";
+    private static final String TEXGYREHEROS = "texgyreheros-regular";
+    private static final String NOTOSANS = "NotoSans-Regular";
+    private static final String IBMPLEXSANS = "IBMPlexSans-Regular";
+
+    private static final String[] TTF_LIST = {
+            HELVETICA,
+            DEJAVUSANS,
+            TEXGYREHEROS,
+            NOTOSANS,
+            IBMPLEXSANS,
+    };
+
+    private static final Map<String, Integer> ERRORI;
+    static {
+        ERRORI = new HashMap<>();
+        ERRORI.put(HELVETICA, 2);
+        ERRORI.put(DEJAVUSANS, 0);
+        ERRORI.put(TEXGYREHEROS, 4);
+        ERRORI.put(NOTOSANS, 2);
+        ERRORI.put(IBMPLEXSANS, 3);
+    }
+
+    private static final Properties PATTERNS = PropsIO.loadFromClassLoaderSafe( "font/patterns.properties" );
 
     @Test
     void testWithPdfBoxFontHelvetica() throws IOException {
         SpecialCharacters sp = new SpecialCharacters();
-        for ( int k=0; k<SPECIAL_CHARACTERS.length; k++ ) {
-            String special = SPECIAL_CHARACTERS[k];
+        String[] specials = SPECIAL_CHARACTERS;
+        for ( int k=0; k<specials.length; k++ ) {
+            String special = specials[k];
             String line = String.format( TEST_LINE, special );
             try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
                 log.info( "testWithPdfBoxFontHelvetica index:{}, char:{}, line:{}", k, special, line );
@@ -52,23 +88,34 @@ class TestSpecialCharacters {
     }
 
     @Test
-    void testWithExternalHelvetica() throws IOException {
-        // some characters are still not available on all font, for instance in this example :
-        // "\uFFFD",   // '.notdef'
-        // "\u25AA",   // 'H18543'
+    void testWithExternalFonts() throws IOException {
         SpecialCharacters sp = new SpecialCharacters();
-        for ( int k=0; k<SPECIAL_CHARACTERS.length; k++ ) {
-            String special = SPECIAL_CHARACTERS[k];
-            String line = String.format( TEST_LINE, special );
-            File pdfFile = new File( String.format( "target/test_font_external_%s.pdf", k ) );
-            log.info( "pdfFile: {}, delete: {}", pdfFile, pdfFile.delete() );
-            Assertions.assertFalse( pdfFile.exists() );
-            try (InputStream fontIS = ClassHelper.loadFromClassLoader( "font/Helvetica.ttf" );
-                 OutputStream os = new FileOutputStream( pdfFile ) ) {
-                log.info( "testWithExternalHelvetica index:{}, char:{}, line:{}", k, special, line );
-                sp.generatePDF(os, fontIS, line);
+        for( String ttf : TTF_LIST ) {
+            String fontPath = String.format( "font/%s.ttf", ttf );
+            String[] specials = SPECIAL_CHARACTERS;
+            int countErrors = 0;
+            StringBuilder errorList = new StringBuilder();
+            for ( int k=0; k<specials.length; k++ ) {
+                String special = specials[k];
+                String unicode = String.format( "u%04X", (int)special.charAt( 0 ) );
+                String line = String.format( TEST_LINE, special );
+                File pdfFile = new File( String.format( "target/test_font_%s_%s.pdf", ttf, unicode ) );
+                log.info( "pdfFile: {}, delete: {}", pdfFile, pdfFile.delete() );
+                Assertions.assertFalse( pdfFile.exists() );
+                try (InputStream fontIS = ClassHelper.loadFromClassLoader( fontPath );
+                     OutputStream os = new FileOutputStream( pdfFile ) ) {
+                    log.info( "test '{}' index:{}, char:{}, line:{}", ttf, k, special, line );
+                    sp.generatePDF(os, fontIS, line);
+                } catch (Exception e) {
+                    log.error( "error on font : {} - {}", ttf, e.getMessage() );
+                    pdfFile.delete();
+                    errorList.append( '\\' );
+                    errorList.append( unicode );
+                    countErrors++;
+                }
             }
-            Assertions.assertTrue( pdfFile.exists() );
+            log.info( "font : {}, countErrors: {}, errorList : [{}]", ttf, countErrors, errorList );
+            Assertions.assertEquals( ERRORI.get( ttf ), countErrors );
         }
     }
 
@@ -79,6 +126,43 @@ class TestSpecialCharacters {
             sp.generatePDF( os, PDType1Font.HELVETICA, "Normal line" );
             Assertions.assertNotEquals( 0, os.toByteArray().length );
         }
+    }
+
+    private boolean check( String regex, String input ) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+
+    private static final String TEST_STRING_1 = "\u27E8 formula \u27E9";
+
+    private static final String TEST_STRING_2 = "\u2002 test enscape";
+
+    @Test
+    void testHelvetica() {
+        String testString1 = TEST_STRING_1;
+        log.info( "testString1 : {}", testString1 );
+        Assertions.assertTrue( check( PATTERNS.getProperty( HELVETICA ), testString1 ) );
+        Assertions.assertFalse( check( PATTERNS.getProperty( DEJAVUSANS ), testString1 ) );
+        String testString2 = TEST_STRING_2;
+        log.info( "testString2 : {}", testString2 );
+        Assertions.assertTrue( check( PATTERNS.getProperty( TEXGYREHEROS ), testString2 ) );
+        Assertions.assertFalse( check( PATTERNS.getProperty( HELVETICA ), testString2 ) );
+    }
+
+    private String choseFont( String testText ) {
+        for (String ttf : Arrays.asList( HELVETICA, DEJAVUSANS ) ) {
+            if ( !check( PATTERNS.getProperty( ttf ), testText ) ) {
+                return ttf;
+            }
+        }
+        return null;
+    }
+
+    @Test
+    void testChoseFont() {
+        Assertions.assertEquals( DEJAVUSANS, choseFont( TEST_STRING_1 ) );
+        Assertions.assertEquals( HELVETICA, choseFont( TEST_STRING_2 ) );
     }
 
 }
